15d60817572a5a3854aa53677612915e1beff33f
DavidHearl <david.hearl@outlook.com>
2023-12-10 23:02:20 +0000

    updated the create_player_json function to create folders, and also change the names to match the json files downloaded from the squad data tables

--- player_stats.py
+++ player_stats.py
@@ -11,26 +11,9 @@ class PlayerTables:
 		self.database_urls = database_urls
 		self.player_tables = player_tables
 
-	# Create a folder for each team
-	def create_team_folders(self):
-		# Load Regular Season - Overall.json
-		league_table = "raw_data/squad_data/Regular Season - Overall.json"
-
-		with open(league_table, "r") as json_file:
-			data = json.load(json_file)
-
-		# Extract the "Squads" from the JSON data
-		teams = [entry["Squad"] for entry in data]
-
-		for team in teams:
-			# Create a new folder for each team
-			folder_name = os.path.join("raw_data/teams", team)
-			os.makedirs(folder_name, exist_ok=True)
-
-		return teams
-
-	# Download all the squad tables from the homepage
+	# Download all the player tables from the club url
 	def create_player_json(self):
+		# Iterate through all the club urls
 		for url in self.database_urls:
 			# Download the page and convert to HTML
 			html = requests.get(url, timeout=20)
@@ -39,12 +22,46 @@ class PlayerTables:
 			# Initialize BeautifulSoup
 			soup_team_list = BeautifulSoup(home_page, features="lxml")
 
-			# Extract team name from the URL
-			team_name = url.split("/")[-2]
+			# -----------------------------------------------------------------
+			# Create team folders
+
+			# Split the URL by "/"
+			url_parts = url.split("/")
+			
+			# Find the index of 'squads' in the URL
+			squads_index = url_parts.index("squads")
+			
+			# Extract the part of the URL containing the team name
+			team_name_with_hyphen = url_parts[squads_index + 2]
+			
+			# Check if the team name ends with "Stats" and remove it
+			if team_name_with_hyphen.endswith("-Stats"):
+				team_name_with_hyphen = team_name_with_hyphen[:-6]  # Remove the last 6 characters ("Stats")
+			
+			# Remove hyphens from the team name
+			team_name = team_name_with_hyphen.replace('-', ' ')
+			
+			# Convert 'United' to 'Utd'
+			if 'United' in team_name.split():
+				team_name = team_name.replace('United', 'Utd')
+			
+			# Handle special cases
+			special_cases = {
+				'Brighton and Hove Albion': 'Brighton',
+				'West Ham Utd': 'West Ham',
+				'Wolverhampton Wanderers': 'Wolves',
+				'Nottingham Forest': "Nott'ham Forest"
+			}
+			
+			for special_case, replacement in special_cases.items():
+				if special_case in team_name:
+					team_name = team_name.replace(special_case, replacement)
 
 			# Create a new folder for each team
 			folder_name = os.path.join("raw_data/teams", team_name, "player_data")
-			os.makedirs(folder_name, exist_ok=True)
+			os.makedirs(folder_name, exist_ok=True)		
+
+			# -----------------------------------------------------------------
 
 			# Iterate through the player tables
 			for i in range(len(self.player_tables)):
@@ -56,8 +73,7 @@ class PlayerTables:
 				table_data = pd.read_html(str(data))[0]
 
 				# Create a .JSON file using the strings from player table
-				json_filename = os.path.join(folder_name, f"{self.player_tables[i]}.json")
-				print(json_filename)
+				json_filename = f"{self.player_tables[i]}.json"
 
 				# Open each .JSON file and convert tables to JSON data
 				try:
@@ -70,7 +86,24 @@ class PlayerTables:
 database_urls = [
 	"https://fbref.com/en/squads/18bb7c10/Arsenal-Stats",
 	"https://fbref.com/en/squads/8602292d/Aston-Villa-Stats",
-	"https://fbref.com/en/squads/4ba7cbea/Bournemouth-Stats"
+	"https://fbref.com/en/squads/4ba7cbea/Bournemouth-Stats",
+	"https://fbref.com/en/squads/cd051869/Brentford-Stats",
+	"https://fbref.com/en/squads/d07537b9/Brighton-and-Hove-Albion-Stats",
+	"https://fbref.com/en/squads/943e8050/Burnley-Stats",
+	"https://fbref.com/en/squads/cff3d9bb/Chelsea-Stats",
+	"https://fbref.com/en/squads/47c64c55/Crystal-Palace-Stats",
+	"https://fbref.com/en/squads/d3fd31cc/Everton-Stats",
+	"https://fbref.com/en/squads/fd962109/Fulham-Stats",
+	"https://fbref.com/en/squads/822bd0ba/Liverpool-Stats",
+	"https://fbref.com/en/squads/e297cd13/Luton-Town-Stats",
+	"https://fbref.com/en/squads/b8fd03ef/Manchester-City-Stats",
+	"https://fbref.com/en/squads/19538871/Manchester-United-Stats",
+	"https://fbref.com/en/squads/b2b47a98/Newcastle-United-Stats",
+	"https://fbref.com/en/squads/e4a775cb/Nottingham-Forest-Stats",
+	"https://fbref.com/en/squads/1df6b87e/Sheffield-United-Stats",
+	"https://fbref.com/en/squads/361ca564/Tottenham-Hotspur-Stats",
+	"https://fbref.com/en/squads/7c21e445/West-Ham-United-Stats",
+	"https://fbref.com/en/squads/8cec06e1/Wolverhampton-Wanderers-Stats"
 ]
 
 player_tables = [
