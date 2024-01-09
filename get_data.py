db394a22bc83047fb219b0d2c5c9c19aafef2dea
DavidHearl <david.hearl@outlook.com>
2024-01-09 14:11:19 +0000

    modified get player data to take the new urls and the current season variable

--- get_data.py
+++ get_data.py
@@ -17,7 +17,7 @@ season = '2023-2024'
 club_stats = ClubStatistics(season)
 
 # Get Player Statistics
-player_stats = PlayerStatistics()
+player_stats = PlayerStatistics(season)
 
 # Get Fixtures
 fixture_list = MatchHistory()
@@ -63,8 +63,8 @@ class StatsProcessor:
 stats_processor = StatsProcessor(club_stats, player_stats, fixture_list)
 
 # Call the methods on the instance, which are now wrapped with the timing_decorator
-stats_processor.process_club_stats()
-# stats_processor.process_player_stats()
+# stats_processor.process_club_stats()
+stats_processor.process_player_stats()
 # stats_processor.process_fixtures()
 # stats_processor.process_remove_extra_data()
 # stats_processor.process_create_match_folders()
--- get_data/get_player_statistics.py
+++ get_data/get_player_statistics.py
@@ -7,8 +7,14 @@ from bs4 import BeautifulSoup
 import io
 
 class PlayerStatistics:
+	def __init__(self, season):
+		self.season = season
+
 	# Download all the player tables from the club url
 	def create_json(self):
+		# Set the base url
+		base_url = "https://fbref.com"
+
 		# Print a blank line to separate the output
 		print()
 
@@ -17,14 +23,17 @@ class PlayerStatistics:
 			data = json.load(f)
 
 		# Assign the club_urls to a variable
-		club_urls = data['club_urls']
+		club_urls = data['club_urls'][self.season]
 		player_statistics_tables = data['player_statistics_tables']
 
 		# Iterate through all the club urls
 		for url in club_urls:
 			with requests.Session() as session:
+				# Join the base_url with the club url
+				club_url = base_url + url
+
 				# Download the page and convert to HTML
-				html = session.get(url, timeout=20)
+				html = session.get(club_url, timeout=20)
 
 				# Initialize BeautifulSoup
 				soup_team_list = BeautifulSoup(html.text, features="lxml")
@@ -61,7 +70,7 @@ class PlayerStatistics:
 				team_name = special_cases.get(team_name, team_name)
 
 				# Create a new folder for each team
-				folder_name = os.path.join("raw_data/2023-2024/player_data", team_name)
+				folder_name = os.path.join(f"raw_data/{self.season}/player_data", team_name)
 				os.makedirs(folder_name, exist_ok=True)		
 
 				# Iterate through the player tables
@@ -77,7 +86,7 @@ class PlayerStatistics:
 					table_data = pd.read_html(io.StringIO(str(data)))[0]
 
 					# Create a .JSON file using the strings from player table
-					json_filename = os.path.join("raw_data/2023-2024/player_data", team_name, f"{player_statistics_tables[i]}.json")
+					json_filename = os.path.join(f"raw_data/{self.season}/player_data", team_name, f"{player_statistics_tables[i]}.json")
 					print(json_filename)
 
 					# Create the directory if it doesn't exist
