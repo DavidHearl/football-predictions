1ad738836af582505454298429615bfb9c0ee713
DavidHearl <david.hearl@outlook.com>
2024-01-26 20:38:48 +0100

    ran get_legacy_data and adjusted print statements

--- get_data/get_legacy/get_legacy_player_statistics.py
+++ get_data/get_legacy/get_legacy_player_statistics.py
@@ -22,9 +22,6 @@ class LegacyPlayerStatistics:
 		with open('get_data/keys.json', 'r') as f:
 			data = json.load(f)
 
-		# Print a blank line to separate the output
-		print()
-
 		# Assign the player statistics tables to a variable
 		player_statistics_tables = data['player_statistics_tables']
 
@@ -34,44 +31,51 @@ class LegacyPlayerStatistics:
 		for league in club_urls:
 			for season in self.legacy_seasons:
 				for item in club_urls[league][season]:
+					print()
+					print("Current Time:", time.strftime("%H:%M:%S", time.localtime()))
+
 					# Add a delay to stop the server from blocking the request
-					time.sleep(4)
+					print("Waiting 5 seconds...")
+					time.sleep(5)
+					print()
 					
 					# Add a delay to try again if the request fails
 					while True:
 						try:
 							# Download the page and convert to HTML
-							html = requests.get(item[0], timeout=20)
+							html = requests.get(item[0], timeout=30)
+							print(item[0])
 							break
 						except requests.exceptions.Timeout:
 							time.sleep(900) # Wait 15 minutes before trying again
 							print("Timeout occurred. Trying again in 15 minutes...")
 							print("Current Time:", time.strftime("%H:%M:%S", time.localtime()))
 
-						# Create a new folder for each team
-						folder_name = os.path.join(f"raw_data/{league}/{season}/player_data", item[1])
-						os.makedirs(folder_name, exist_ok=True)		
-						print(folder_name)
+					# Initialize BeautifulSoup
+					soup_team_list = BeautifulSoup(html.text, features="lxml")
 
-						# Iterate through the player tables
-						for i in range(len(player_statistics_tables)):
-							# Iterate through the 'stats table'
-							# 'stats table' is the class of the table element
-							data = soup_team_list.select('table.stats_table')[i]
+					# Create a new folder for each team
+					folder_name = os.path.join(f"raw_data/{league}/{season}/player_data", item[1])
+					os.makedirs(folder_name, exist_ok=True)		
+					print(folder_name)
 
-							# Read the table using Pandas
-							table_data = pd.read_html(io.StringIO(str(data)))[0]
+					# Use list comprehension to iterate over the tables
+					tables = [
+						pd.read_html(io.StringIO(str(data)))[0]
+						for data in soup_team_list.select('table.stats_table')
+					]
 
-							# Create a .JSON file using the strings from player table
-							json_filename = os.path.join(folder_name, f"{player_statistics_tables[i]}.json")
-							print(json_filename)
+					for table_name, table in zip(player_statistics_tables, tables):
+						# Create a .JSON file using the strings from squad table
+						json_filename = os.path.join(folder_name, table_name + ".json")
+						print(json_filename)
 
-							# Create the directory if it doesn't exist
-							os.makedirs(os.path.dirname(json_filename), exist_ok=True)
+						# Create the directory if it doesn't exist
+						os.makedirs(os.path.dirname(json_filename), exist_ok=True)
 
-							# Open each .JSON file and convert tables to JSON data
-							try:
-								with open(json_filename, "w") as json_file:
-									json.dump(json.loads(table_data.to_json(orient="records")), json_file, indent=4)
-							except Exception as e:
-								print(f"Error: {e}")
+						# Open each .JSON file and convert tables to JSON data
+						try:
+							with open(json_filename, "w") as json_file:
+								json.dump(json.loads(table.to_json(orient="records")), json_file, indent=4)
+						except Exception as e:
+							print(f"Error: {e}")
